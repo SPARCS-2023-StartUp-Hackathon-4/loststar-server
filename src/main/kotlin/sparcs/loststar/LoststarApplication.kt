1417d1af7ac0637b9dae675111ea61e4554c7579
sectionr0 <sectionr0@gmail.com>
2023-01-27 20:40:51 +0900

    [FEAT] init Commit

---
+++ src/main/kotlin/sparcs/loststar/LoststarApplication.kt
@@ -0,0 +1,11 @@
+package sparcs.loststar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoststarApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoststarApplication>(*args)
+}
---
+++ src/test/kotlin/sparcs/loststar/LoststarApplicationTests.kt
@@ -0,0 +1,13 @@
+package sparcs.loststar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoststarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
